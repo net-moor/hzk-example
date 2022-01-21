2948413fe70f3cef49b9ec70f33987de1cb9bddd
Batov_Nikolay <batovna@gmail.com>
2022-01-21 22:58:08 +0300

    fix docker 9

--- .space.kts
+++ .space.kts
@@ -3,14 +3,16 @@
 job("Build and push Docker") {
     docker {
         build {
-            context = "/mnt/space/work/hazelcast-demo"
-            file = "./Dockerfile"
+            context = "docker"
+            file = "./docker/Dockerfile"
+            args["HTTP_PROXY"] = "http://10.20.30.2:1234"
             labels["vendor"] = "netmoor"
-            args["HTTP_PROXY"] = "http://10.20.30.1:123"
         }
 
-        push("netmoor.registry.jetbrains.space/p/edu/containers/hzk-demo") {
-            tags(tags("0.0.run_number"))
+        push("mycompany.registry.jetbrains.space/p/projectkey/mydocker/myimage") {
+            // use current job run number as a tag - '0.0.run_number'
+            tags("0.0.run_number")
+            // see example on how to use branch name in a tag
         }
     }
 }
